f6905e93535ff9c4fcedcb2db95136e918de675c
tom-schier <schiertxs@gmail.com>
2018-09-13 20:54:15 +1000

    First Entry with FB authentication

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,44 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCheckboxModule, MatButtonToggleModule, MatTabsModule, MatFormFieldModule} from '@angular/material';
+import {MatMenuModule} from '@angular/material/menu';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { AuthService } from '../services/security/auth.service';
+import { UserService } from '../services/user/user.service';
+import { AuthGuard } from '../services/security/auth.guard';
+import { rootRouterConfig } from './app.routes';
+import { UserResolver } from './user/user.resolver';
+import { UserComponent } from './user/user.component';
+import { AngularFireModule } from '@angular/fire';
+import { environment } from '../environments/environment';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    UserComponent,
+    MainComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule, MatCheckboxModule, MatMenuModule, MatButtonToggleModule, MatTabsModule, MatFormFieldModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(rootRouterConfig, { useHash: false }),
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule, // imports firebase/firestore, only needed for database features
+    AngularFireAuthModule, // imports firebase/auth, only needed for auth features
   ],
-  providers: [],
+  providers: [AuthService, UserService, UserResolver, AuthGuard, AuthService, UserService, UserResolver, AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/main/main.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/security/auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.css']
+})
+export class MainComponent implements OnInit {
+
+  constructor(private _authSvc: AuthService, private router: Router) { }
+
+  ngOnInit() {
+  }
+
+  doLogout(){
+    this._authSvc.doLogout().then(val => {
+      this.router.navigate(['/login']);
+      console.log("Logout successful");
+    })
+  }
+
+}
